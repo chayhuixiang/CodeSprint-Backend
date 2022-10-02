aed48f31846918b47042c50011b9130097a7dd59
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2022-10-02 12:52:57 +0800

    Commit object detection

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.9.6-slim
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN apt-get update
+
+RUN apt-get install ffmpeg libsm6 libxext6 cmake -y
+
+RUN pip install --upgrade pip
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+EXPOSE 30123
+
+ENTRYPOINT [ "python" ]
+
+CMD ["main.py"]
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from detect import detect
+import base64
+import os
+import glob
+import shutil
+
+
+app = Flask(__name__)
+
+
+@app.route('/scoreimgs', methods=['POST'])
+def get_score():                    
+    img = request.json['image']
+    #img = base64.b64encode(open("./images.jpeg", "rb").read())
+    with open("./data/images/img.png", "wb") as fh:
+        fh.write(base64.b64decode((img)))
+    results = detect()
+    
+    upload = glob.glob('./data/images/*')
+    for f in upload:
+        os.remove(f)
+    shutil.rmtree("./runs/detect", ignore_errors=False)
+    print("------")
+    print(results)
+    return results
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=30123)
+
